fd95cc75c912ddf566034099a13b00fc9a58adfe
hramn1 <stmihailov@yandex.ru>
2019-02-12 01:01:16 +0300

    правки

--- schema.sql
+++ schema.sql
@@ -1,5 +1,6 @@
 CREATE DATABASE doingsdone
-  DEFAULT CHARACTER SET utf8;
+  DEFAULT CHARACTER SET utf8
+  DEFAULT COLLATE utf8_general_ci;
   USE doingsdone;
 CREATE TABLE projects (
  id INT AUTO_INCREMENT PRIMARY KEY,
@@ -9,18 +10,20 @@ CREATE TABLE projects (
 CREATE TABLE tasks (
  id INT AUTO_INCREMENT PRIMARY KEY,
  сreate_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
- complete_date TIMESTAMP,
+ complete_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
  status INT DEFAULT 0 CHECK (status=0 and status=1),
- name CHAR(64),
+ name CHAR(64) UNIQUE,
  file CHAR(128),
- expire_date TIMESTAMP,
+ expire_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
  user_id INT,
  project_id INT
 );
 CREATE TABLE users (
  id INT AUTO_INCREMENT PRIMARY KEY,
  reg_dt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
- email CHAR(64) NOT NULL,
+ email CHAR(64) NOT NULL UNIQUE,
  usr_name CHAR(128),
  usr_pass CHAR(64)
 );
+ CREATE UNIQUE INDEX t_name ON tasks(name);
+ CREATE UNIQUE INDEX c_email ON users(email);
