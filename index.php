4ca4406592a2b0e09f350a5da5bef20a4bc26afe
stas <stmihailov@yandex.ru>
2019-02-20 14:47:52 +0300

    правки

--- index.php
+++ index.php
@@ -4,29 +4,28 @@
   require('functions.php');
   $link = mysqli_connect('localhost', 'doing', '300685', 'doingsdone') or die (mysqli_connect_error($link));
   mysqli_set_charset($link, "utf8");
-  $user = resultArray($link, 'SELECT usr_name FROM users WHERE id = ?',[1]);
-  $projects = resultArray($link, 'SELECT * FROM projects WHERE user_id = ?',[1]);
-  $tasks = resultArray($link, 'SELECT * FROM tasks WHERE user_id = ?',[1]);
+  $data =[1];
+  $user = resultArray($link, 'SELECT usr_name FROM users WHERE id = ?',$data);
+  $projects = resultArray($link, 'SELECT * FROM projects WHERE user_id = ?',$data);
+  $tasks = resultArray($link, 'SELECT * FROM tasks WHERE user_id = ?',$data);
   if (isset($_GET['id'])) {
     $id = (int) $_GET['id'];
-    foreach ($tasks as $key => $value) {
-        if ($id == $value['id']) {
-          print ($id);
-          $tasks_project = resultArray($link, 'SELECT * FROM tasks WHERE project_id = ? AND user_id = ?', [$id, 1] );
-        }
-        else {
-            http_response_code(404);
-        }
-    }
+    $data = [$id, 1];
+    $tasks_project = resultArray($link, 'SELECT * FROM tasks WHERE project_id = ? AND user_id = ?', $data );
+  }
+  else {
+    $tasks_project = $tasks;
 }
-else {
-  $tasks_project = $tasks;
-}
+if($tasks_project){
   $page_content = include_template('index.php', [
         'tasks' => $tasks,
   			'show_complete_tasks' => $show_complete_tasks,
         'tasks_project' => $tasks_project
   			]);
+      }
+      else {
+        $page_content = 'ERROR 404';
+  }
   $layout_content = include_template('layout.php', [
         'tasks' => $tasks,
         'user' => $user,
