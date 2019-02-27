8d1134988c2eec6eddf3d0fac1fd9060ed763bba
hramn1 <stmihailov@yandex.ru>
2019-02-28 00:13:21 +0300

    правки

--- auth.php
+++ auth.php
@@ -18,31 +18,32 @@ $errors = [];
         $errors[$key] = 'Это поле надо заполнить';
       }
     }
-        // Проверка полей
-        if ((!filter_var($data['email'], FILTER_VALIDATE_EMAIL) or strlen($data['email']) > 128)) {
-            $errors['email'] = 'E-mail введён некорректно';
-        }
-        if (empty($errors)) {
-            $email = $data['email'];
-            $dataSql = [$email];
+    // Проверка полей
+    if ((!filter_var($data['email'], FILTER_VALIDATE_EMAIL) or strlen($data['email']) > 128)) {
+        $errors['email'] = 'E-mail введён некорректно';
+    }
+    if (empty($errors)) {
+        $email = $data['email'];
+        $dataSql = [$email];
 //            $sql = 'SELECT * FROM users WHERE email = "' . $email . '"';
 //            $res = mysqli_query($link, $sql);
 //            $user = $res ? mysqli_fetch_array($res, MYSQLI_ASSOC) : null;
-             $users = resultArray($link, 'SELECT * FROM users WHERE email = ?', $dataSql );
-                         if ($users === []) {
-                             $errors['email'] = 'Такой пользователь не найден';
-                         } else {
-             foreach ($users as $user){
-            if (password_verify($data['password'], $user['usr_pass'])) {
-                $_SESSION['user'] = $user;
-                header("Location: /");
-                   exit();
+         $users = resultArray($link, 'SELECT * FROM users WHERE email = ?', $dataSql );
+         if ($users === []) {
+             $errors['email'] = 'Такой пользователь не найден';
+         }
+         else {
+                foreach ($users as $user){
+                    if (password_verify($data['password'], $user['usr_pass'])) {
+                        $_SESSION['user'] = $user;
+                        header("Location: /");
+                           exit();
+                    }
+                    else {
+                        $errors['password'] = 'Неверный пароль';
+                    }
+                }
             }
-            else {
-                $errors['password'] = 'Неверный пароль';
-             }
-        }
-        }
         }
     }
     $page_content = include_template('auth.php', [
